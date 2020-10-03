48758e27b3ef67e140118e6bf6eecbf507ff33d8
Pavlo Sliusarchuk <sksuzh@gmail.com>
2020-10-03 15:48:11 +0300

    feat: add dream-tem implementation

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members =[]) {
+  //throw new CustomError('Not implemented');
+ 
+  if(
+    !members ||
+    members.length ===0 ||
+    !Array.isArray(members)
+  ) return false;
+  
+  let nameTeam = members.reduce((res, item) => {
+    if(typeof item !== 'string' || !item.length || !item.trim() ) return res;
+    const itemTrimmed = item.trim();
+    res.push(itemTrimmed[0].toUpperCase());
+    return res;
+  }, []);
+  
+return nameTeam.sort().join('');
 };
