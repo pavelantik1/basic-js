f7cb8610a5663de80af5e2b83cdddf936cac4db6
Pavlo Sliusarchuk <sksuzh@gmail.com>
2020-10-03 21:59:11 +0300

    feat: add hganoi-towers implementation

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+ return{
+  'turns': 2**disksNumber-1,
+  'seconds': Math.floor((2**disksNumber-1)*3600/turnsSpeed)
+}
+
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  //throw new CustomError('Not implemented');
+  throw new CustomError('Not implemented');
   if(date === undefined) return 'Unable to determine the time of year!'
   
   const month = date.getMonth();
